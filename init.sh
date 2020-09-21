867d6cea9b22ad47a224b434b633ca44ec2f065b
XanderCodes <xanderstuff7@gmail.com>
2020-09-22 03:40:57 +1000

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR=$PWD
 curl -s https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz | tar -vxz
 cp -f linux64/steamclient.so Unturned_Headless_Data/Plugins/x86_64/steamclient.so
+ln -s linux64/steamclient.so /home/steam/.steam/sdk64/steamclient.so
 
 # Update / install server
 ./steamcmd.sh +login $STEAM_USERNAME $STEAM_PASSWORD $STEAM_GUARD_TOKEN $STEAM_CMD_ARGS +force_install_dir $GAME_INSTALL_DIR +@sSteamCmdForcePlatformBitness 64 +app_update $GAME_ID +quit
